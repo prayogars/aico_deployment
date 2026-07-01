ce399b0869cde977a53ecccd2658102370349129
prayogars <52367627+prayogars@users.noreply.github.com>
2026-07-01 19:53:22 +0700

    remove icon from main

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import streamlit as st
 
 # Define individual pages
-home_page = st.Page("Eda.py", title="Data & AI Job Market", icon=":woman_technologist:")
-analytics_page = st.Page("analytics.py", title="Analytics", icon=":brain:")
+home_page = st.Page("Eda.py", title="Data & AI Job Market")
+analytics_page = st.Page("analytics.py", title="Analytics")
 
 # Create the navigation menu
 pg = st.navigation([home_page, analytics_page])
